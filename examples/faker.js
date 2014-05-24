ce056b84ba2c4aa46069a810ec5792982223540c
bibig <liushuxun@gmail.com>
2014-05-24 15:06:30 +0800

    use new filler version to faker whole db, including images

--- examples/faker.js
+++ examples/faker.js
@@ -1,33 +1,51 @@
 
 var path   = require('path');
 var Filler = require('filler');
+var async  = require('async');
 
 function dropDB (fn) {
-  var exec = require('child_process').exec;
-  var command = 'rm -rf ' + path.join(__dirname, 'data');
+  console.log('ready clear db');
+  clearFiles(path.join(__dirname, 'data'), fn);
+}
 
-  console.log('ready to drop db');
-  exec(command, function(err, stdout, stderr) {
-    fn();
-  });
+function clearFiles (path, fn) {
+  var exec = require('child_process').exec;
+  var command = 'rm -rf ' + path;
+
+  exec(command, function(e, stdout, stderr) {
+    fn(e);
+  }); 
+}
+
+function clearImages (fn) {
+  console.log('ready clear images');
+  clearFiles(path.join(__dirname, './public/uploads/articles/*'), fn);
 }
 
 function rock () {
   var can = require('./can');
   var filler = new Filler(can, {
+    image_resources: path.join(__dirname, './images'),
     lang: 'en',
     tables: {
       site: 1,
       articleCategories: 10,
-      articles: 500
+      articles: 100,
+      articleImages: 230
     }
   });
 
   console.log('ready to fill db');
-  filler.run();
-  console.log('done');
+  filler.run(function () {
+    console.log('done');  
+  });
 }
 
-dropDB(function () {
-  rock();
+async.series([
+  dropDB,
+  clearImages,
+], function (e) {
+  if (e) {console.error(e);} else {
+    rock();
+  }
 });
